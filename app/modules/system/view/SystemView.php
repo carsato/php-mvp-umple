90b6c2b77d67ce4f5bd88a59cf96ce40f266d057
Carlos Saport Tomás <carlos.saport@gmail.com>
2015-03-31 00:58:41 +0200

    try to user StampTE from gabordemooij

--- app/modules/system/view/SystemView.php
+++ app/modules/system/view/SystemView.php
@@ -1,12 +1,45 @@
 <?php
 
+
 class SystemView{
   public function view(){
     return array(
-      '<div>',
-      __class__.' '.__function__.' div1',
-      __class__.' '.__function__.' div2',
-      ' </div>',
+      '#render' => array(
+        '#module' => 'System',
+        '#view'   => 'SystemView',
+        '#method' => 'stampteRender',
+        '#args' => array(
+          'template' => " <table>
+                              <thead>
+                                  <tr><th>Pizza</th><th>Price</th></tr>
+                              </thead>
+                              <tbody>
+                              <!-- cut:pizza -->
+                                  <tr><td>
+                                  #name#
+                                  </td><td>
+                                  #price#
+                                  </td></tr>
+                              <!-- /cut:pizza -->
+                              </tbody>
+                          </table>",
+          'data' => array(
+            'Margherita' => '7.00',
+            'Funghi' => '7.50',
+            'Tonno' => '8.00',
+          ),
+        ),
+      ),
     );
+  }
+
+  public function stampteRender(){
+    require dirname(__file__).'/../../../vendor/gabordemooij/stamp/StampTE.php';
+    $args = func_get_args();
+    $template = $args['template'];
+    $data = $args['data'];
+    $tpl = new StampTE($template);
+    foreach($data as $name=>$value) {
+    }
   }
 }
